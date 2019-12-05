eb2e956887e7e9a216619145473928a4ea5f2def
Andrii Davnich <d.a.lonerwolf@gmail.com>
2019-12-05 21:15:40 +0200

    +header

--- src/js/main.js
+++ src/js/main.js
@@ -15,5 +15,13 @@ loadData().then(() => {
 
 document.querySelector('.portfolio').onclick = () => {
 	document.querySelector('.wrapSlideshow').classList.add('wrapSlideshow_off');
-	document.querySelector('header').classList.add('active');
+	document.querySelector('.wrap-main-page').classList.add('active');
+}
+
+document.querySelector('.logo').onclick = backHomePage;
+document.querySelector('#home').onclick = backHomePage;
+
+function backHomePage() {
+	document.querySelector('.wrapSlideshow').classList.remove('wrapSlideshow_off');
+	document.querySelector('.wrap-main-page').classList.remove('active');
 }
